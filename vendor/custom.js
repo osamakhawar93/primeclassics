99998db43965e42ba9ed4437479cdfe0c586db9b
Nira Naveed <niranaveed01@gmail.com>
2018-11-02 03:03:47 +0500

    committing code services page

--- vendor/custom.js
+++ vendor/custom.js
@@ -20,4 +20,24 @@ $(document).ready(function() {
       <i class="fa fa-caret-right grayclr"></i>\
   </button>'
   });
+
+  $(".nn-tabs .nav-item").click(function() {
+    $(".nn-tabs .nav-item div").removeClass("border-btm-active");
+    $(this)
+      .find("div")
+      .addClass("border-btm-active");
+  });
+
+  $(".our-services-slider").slick({
+    nav: true,
+    dots: true,
+    prevArrow:
+      '<button class="left-arrow">\
+      <i class="fa fa-caret-left grayclr"></i>\
+  </button>',
+    nextArrow:
+      '<button class="right-arrow">\
+      <i class="fa fa-caret-right grayclr"></i>\
+  </button>'
+  });
 });
